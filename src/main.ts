3b8771b4e7babe6b762232f49d773cb7d3f7e48f
Lior Avigdor <lior@snowmate.io>
2022-12-20 15:09:13 +0200

    Init commit

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,22 @@
 import * as core from '@actions/core';
+import * as github from '@actions/github';
 
-const time = (new Date()).toTimeString();
-core.setOutput("outputName", time);
+    // get token for octokit
+    const token = core.getInput('repo-token');
+    const octokit = github.getOctokit(token)
+
+
+    // call octokit to create a check with annotation and details
+   octokit.rest.checks.create({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        name: 'Readme Validator',
+        head_sha: github.context.sha,
+        status: 'completed',
+        conclusion: 'failure',
+        output: {
+            title: 'README.md must start with a title',
+            summary: 'Please use markdown syntax to create a title',
+        }
+    });
+   
