20ebeb60f00db7c3d1e6dd2c47b6932fcd91c589
Lior Avigdor <lior@snowmate.io>
2023-01-03 14:58:34 +0200

    Check

--- dist/index.js
+++ dist/index.js
@@ -35,7 +35,7 @@ const child_process = __importStar(__nccwpck_require__(81));
 // // get token for octokit
 // const token = core.getInput('repo-token');
 // const octokit = github.getOctokit(token)
-child_process.exec('pwd && ls', {
+child_process.exec('pwd', {
     cwd: './check'
 }, function (error, stdout, stderr) {
     console.log(error, stdout, stderr);
