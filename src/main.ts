e57c9b5b6b5dc94d8dfe0ba0fe7f39511c3c978a
Lior Avigdor <lior@snowmate.io>
2023-01-03 11:56:05 +0200

    Check

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import * as child_process from 'child_process';
     // const token = core.getInput('repo-token');
     // const octokit = github.getOctokit(token)
     let dataToSend  = '5';
-    const python = child_process.spawn('ls');
+    const python = child_process.spawn('pwd');
     // collect data from script
     python.stdout.on('data', function (data) {
      console.log('Pipe data from python script ...');
