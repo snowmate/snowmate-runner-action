3a7a827bf65f2912baafc9a7e0141b93ecf592f4
Lior Avigdor <lior@snowmate.io>
2023-01-03 11:40:29 +0200

    Check

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
+import * as child_process from 'child_process';
 
     // get token for octokit
     const token = core.getInput('repo-token');
     const octokit = github.getOctokit(token)
+
+    const python = child_process.spawn('ls');
+    // collect data from script
+    python.stdout.on('data', function (data) {
+     console.log('Pipe data from python script ...');
+     console.log(data.toString())
+    });
+    // in close event we are sure that stream from child process is closed
+    python.on('close', (code) => {
+    console.log(`child process close all stdio with code ${code}`);
+    // send data to browser
+    });
 
 
     // call octokit to create a check with annotation and details
