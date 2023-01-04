37a6414ee01a214999fcc9ee79b453909e9d3e58
Lior Avigdor <lior@snowmate.io>
2023-01-04 11:51:44 +0200

    Check

--- dist/index.js
+++ dist/index.js
@@ -9653,12 +9653,12 @@ const child_process = __importStar(__nccwpck_require__(2081));
 // get token for octokit
 const token = core.getInput('repo-token');
 const octokit = github.getOctokit(token);
-const result = child_process.execSync('cd regression/check && cat t.py').toString();
+const result = child_process.execSync('cd regression/check && ls && cat t.py').toString();
 // call octokit to create a check with annotation and details
 octokit.rest.checks.create({
     owner: github.context.repo.owner,
     repo: github.context.repo.repo,
-    name: 'Readme Validator',
+    name: 'Snowmate Tests',
     head_sha: github.context.sha,
     status: 'completed',
     conclusion: 'failure',
