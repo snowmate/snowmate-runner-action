a80689f69e58a8df54e7bc093031b3410950fb05
Lior Avigdor <lior@snowmate.io>
2023-01-09 18:12:43 +0200

    Check

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ import * as child_process from 'child_process';
         status: 'completed',
         conclusion: 'failure',
         output: {
-            title: 'README.md must start with a title',
+            title: 'README.md must ssfsfsdftart with a title',
             summary: result,
         }
     });
